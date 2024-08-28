aff4f67f5fa05d8384d86ce1420b7a47ccd8a714
IT24100354 <it24100354@my.sliit.lk>
2024-08-28 10:18:12 +0530

    Add files via upload

---
+++ Q2/IT24100354Lab6Q2A.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class IT24100354Lab6Q2A
+{
+  public static void main(String[] args)
+  {
+     int count;
+     count=1;
+     
+     while(count<=10)
+     {
+       System.out.print(count+" ");  
+       count= count+1;
+      }
+      
+   }
+}    
+       
+     
